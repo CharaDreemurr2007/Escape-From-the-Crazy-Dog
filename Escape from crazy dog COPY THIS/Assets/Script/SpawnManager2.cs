5ceb9d9f393069f2266ba97eae22625ad350f931
CharaDreemurr2007 <bptruong101@student.hbuhsd.edu>
2024-02-26 14:47:26 -0800

    Obstacle can run and human can Jump

---
+++ Escape from crazy dog COPY THIS/Assets/Script/SpawnManager2.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager2 : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(-25, 0, 79);
+    private float startDelay = 3;
+    private float repeatRate = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void SpawnObstacle()
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+    }
+}
