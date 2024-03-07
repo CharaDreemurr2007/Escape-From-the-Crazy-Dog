6b63e816ab14337a254cbeb31ceece713c9da09d
CharaDreemurr2007 <bptruong101@student.hbuhsd.edu>
2024-03-07 14:21:58 -0800

    Camera following

--- Escape from crazy dog COPY THIS/Assets/Script/MoveLeft.cs
+++ Escape from crazy dog COPY THIS/Assets/Script/MoveLeft.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    private float speed = 40;
+    private float speed = 45;
     private float leftBound = 37;
     private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.left * Time.deltaTime * speed);
+        
+        if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
         if(transform.position.x > leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
