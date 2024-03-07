7a3b7f52c73afb36092cfeb923a56efdcd16c12f
CharaDreemurr2007 <bptruong101@student.hbuhsd.edu>
2024-03-07 14:44:07 -0800

    Stop game over

--- Escape from crazy dog COPY THIS/Assets/Script/Movestraight.cs
+++ Escape from crazy dog COPY THIS/Assets/Script/Movestraight.cs
@@ -6,17 +6,22 @@ public class Movestraight : MonoBehaviour
 {
     private float speed = 40;
     private float leftBound = 37;
+    private PlayerController playerControllerScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>(); 
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.back * Time.deltaTime * speed);
+       if(playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * speed);
+        }
         if(transform.position.x > leftBound && gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
